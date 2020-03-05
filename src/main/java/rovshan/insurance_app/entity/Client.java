afac281d1adc06c287f04f0be079a5ab3d56b5f2
PC <rovsen.huseyn.oglu@gmail.com>
2020-03-05 15:49:29 +0400

    -- BiDirectional must be deleted

--- src/main/java/rovshan/insurance_app/entity/Client.java
+++ src/main/java/rovshan/insurance_app/entity/Client.java
@@ -1,11 +1,13 @@
 package rovshan.insurance_app.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @AllArgsConstructor
